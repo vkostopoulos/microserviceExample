0eda27e7b75004221c368c0e45e9b72895421163
vkostopoulos <vaggos_kostopoulos@yahoo.gr>
2020-04-27 23:10:01 +0300

    Add project files.

---
+++ UserService/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UserService.Database;
+using UserService.Database.Entities;
+
+namespace UserService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        private DatabaseContext _databaseContext;
+        public UserController()
+        {
+            _databaseContext = new DatabaseContext();
+        }
+
+        // GET: api/User
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return _databaseContext.Users.ToList();
+        }
+
+        // GET: api/User/5
+        [HttpGet("{id}", Name = "Get")]
+        public User Get(int id)
+        {
+            return _databaseContext.Users.Find(id);
+        }
+
+        // POST: api/User
+        [HttpPost]
+        public IActionResult Post([FromBody] User model)
+        {
+            try
+            {
+                _databaseContext.Users.Add(model);
+                _databaseContext.SaveChanges();
+                return StatusCode(StatusCodes.Status201Created, model);
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, ex);
+            }
+        }
+    }
+}
---
+++ UserService/Database/DatabaseContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserService.Database.Entities;
+
+namespace UserService.Database
+{
+    public class DatabaseContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-5MTTOS6\SQLEXPRESS;" +
+                                                            "Initial Catalog=microserviceExample;" +
+                                                            "persist security info = True;Integrated Security = SSPI;");
+        }
+    }
+}
