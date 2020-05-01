6a93c09675c846bfa8385de052144916ce4681e3
vkostopoulos <vaggos_kostopoulos@yahoo.gr>
2020-05-01 18:18:49 +0300

    Add autoMapper, add load lancer using RoundRobin algorith to ocelot

---
+++ UserServiceReplica/Controllers/UserController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using BOL;
+using DAL.Repository;
+using DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        private readonly IRepository<User> _userRepository;
+        private readonly IMapper _mapper;
+
+        public UserController(IMapper mapper, IRepository<User> userRepository)
+        {
+            _mapper = mapper;
+            _userRepository = userRepository;
+        }
+
+        // GET: api/User
+        [HttpGet]
+        public async Task<IEnumerable<UserDto>> GetAsync()
+        {
+            IEnumerable<User> users = await _userRepository.GetAllAsync();
+            return _mapper.Map<IEnumerable<User>, IEnumerable<UserDto>>(users);
+        }
+
+        // GET: api/User/5
+        [HttpGet("{id}", Name = "Get")]
+        public async Task<UserDto> GetByIdAsync(int id)
+        {
+            User user = await _userRepository.GetByIdAsync(id);
+            return _mapper.Map<User, UserDto>(user);
+        }
+
+        // POST: api/User
+        [HttpPost]
+        public async Task<IActionResult> PostAsync([FromBody] UserDto userDto)
+        {
+            try
+            {
+                User user = _mapper.Map<UserDto, User>(userDto);
+                await _userRepository.InsertAsync(user);
+                return StatusCode(StatusCodes.Status201Created, userDto);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex);
+            }
+        }
+    }
+}
---
+++ UserServiceReplica/Helpers/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BOL;
+using DTO;
+
+namespace UserService.Helpers
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<UserDto, User>();
+            CreateMap<User, UserDto>();
+        }
+    }
+
+}
